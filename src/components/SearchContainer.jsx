5ed8b553721eba3187abbd8cd122321eaa26c0ea
Alex Paglia <alex.paglia.ap@gmail.com>
2024-12-06 20:05:25 -0500

    Get spotify api token pulling in

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const clientId = 'cddf0f9092604c92bb9002d5d6d00cf0'
+const clientSecret = '5d8965fb3e4e4aa9b8ab7d6bb155a8e2'
+let token = ''
 
 export default function SearchContainer() {
+    const [title, setTitle] = useState('')
+    const [authToken, setAuthToken] = useState()
+    
+    //get spotyify API token and set initial state
+    useEffect(() => {
+        axios('https://accounts.spotify.com/api/token', {
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)      
+            },
+            data: 'grant_type=client_credentials',
+            method: 'POST'
+        })
+        .then(tokenResponse => {
+            token = tokenResponse.data.access_token
+            setAuthToken(token)
+        })
+        
+        const interval = setInterval(() => {
+            axios('https://accounts.spotify.com/api/token', {
+                headers: {
+                    'Content-Type' : 'application/x-www-form-urlencoded',
+                    'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)      
+                },
+                data: 'grant_type=client_credentials',
+                method: 'POST'
+            })
+            .then(tokenResponse => {
+                token = tokenResponse.data.access_token
+                setAuthToken(token)
+            })
+        }, 300000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    //handle form submit by grabbing current token and using it for an api call to spotify, then map the results to a song array if they match search criteria
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
                     type='text' 
                     placeholder='Search for a song title'
                     name='title'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <input 
                     type='submit' 
