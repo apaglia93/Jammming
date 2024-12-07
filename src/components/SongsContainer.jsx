1257596204aa013c670512c7bd4b5ca23b6173c3
Alex Paglia <alex.paglia.ap@gmail.com>
2024-12-06 22:45:15 -0500

    Set playlist state on songscontainer component

--- src/components/SongsContainer.jsx
+++ src/components/SongsContainer.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 
 export default function SongsContainer(props) {
+    const [playlistSongs, setPlaylistSongs] = useState([])
     const [songList, setSongList] = useState()
     useEffect(() => {
         setSongList(props.songList)
@@ -14,13 +15,25 @@ export default function SongsContainer(props) {
                     <p className='song-title'>{song.name}</p>
                     <p>{song.artists[0].name}</p>
                 </div>
-                <p className="add-to-playlist">+</p>
+                <p className="add-to-playlist" id={song.id} onClick={handleAddClick}>+</p>
             </div>
         )
+    }
+
+    const handleAddClick = (e) => {
+        if(playlistSongs.includes(e.target.id)) {
+            return
+        }
+
+        setPlaylistSongs([
+            ...playlistSongs, 
+            e.target.id
+        ])
     }
     
     return (
         <>
+            {playlistSongs.length > 0 ? playlistSongs : 'place'}
             <div className='outcome-div'>
                 <h1 className='section-title'>Search Results</h1>
                 {songList ? createSongContainers(songList) : <p style={{color: 'darkblue', padding: '1rem', textAlign: 'center'}}>Run a search to display some songs!</p>}
