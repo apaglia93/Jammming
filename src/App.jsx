da1016b36e3d191f23e047b0850f31fe6109a0ea
Alex Paglia <alex.paglia.ap@gmail.com>
2024-12-06 22:04:00 -0500

    Set up song pulls

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
 import Header from './components/Header'
@@ -7,12 +7,17 @@ import SongsContainer from './components/SongsContainer'
 import PlaylistContainer from './components/PlaylistContainer'
 
 function App() {
+  const [songList, setSongList] = useState()
+  const songListFromSearch = (songs) => {
+    setSongList(songs)
+  }
+  
   return (
     <>
       <Header />
-      <SearchContainer />
+      <SearchContainer setSongList={songListFromSearch} />
       <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
-        <SongsContainer />
+        <SongsContainer songList={songList}/>
         <PlaylistContainer />
       </div>
     </>
--- src/components/SongsContainer.jsx
+++ src/components/SongsContainer.jsx
@@ -1,35 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-const songs = [{
-    title: 'song1',
-    artist: 'artist1',
-    album: 'album1'
-}, {
-    title: 'song2',
-    artist: 'artist2',
-    album: 'album2'
-}, {
-    title: 'song3',
-    artist: 'artist3',
-    album: 'album3'
-}]
+export default function SongsContainer(props) {
+    const [songList, setSongList] = useState()
+    useEffect(() => {
+        setSongList(props.songList)
+        console.log(songList)
+    }, [props.songList])
 
-export default function SongsContainer() {
-    const songContainers = songs.map(song => 
+    const createSongContainers = (songList) => {
+        return songList.map(song => 
+            <div className='song-container'>
+                <div>
+                    <p className='song-title'>{song.name}</p>
+                    <p>{song.artists[0].name}</p>
+                </div>
+                <p className="add-to-playlist">+</p>
+            </div>
+        )
+    }
+    
+    /*const songContainers = songs.map(song => 
         <div className='song-container'>
             <div>
-                <p className='song-title'>{song.title}</p>
-                <p>{song.artist}</p>
+                <p className='song-title'>title</p>
+                <p>artist</p>
             </div>
             <p className="add-to-playlist">+</p>
         </div>
-    )
+    )*/
     
     return (
         <>
             <div className='outcome-div'>
                 <h1 className='section-title'>Search Results</h1>
-                {songs.length > 0 ? songContainers : <p style={{color: 'darkblue', padding: '1rem', textAlign: 'center'}}>Run a search to display some songs!</p>}
+                {songList ? createSongContainers(songList) : <p style={{color: 'darkblue', padding: '1rem', textAlign: 'center'}}>Run a search to display some songs!</p>}
             </div>
         </>
     )
